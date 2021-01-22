e752f983f6d3466ec7b725e080f624751b2d5945
Thomas Claudius Huber <thomas@thomasclaudiushuber.com>
2021-01-22 22:57:06 +0100

    Add project files

---
+++ WinUI.Desktop.Extensions/WindowExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System;
+using System.Runtime.InteropServices;
+
+using WinRT;
+
+using Microsoft.UI.Xaml;
+using Windows.ApplicationModel;
+
+namespace WinUI.Desktop.Extensions
+{
+  public static class WindowExtensions
+  {
+    public static void SetWidth(this Window window, int width)
+    {
+      var hwnd = window.GetWindowHandle();
+      var dpiScaledWidth = (int)(width * window.GetDpiScalingFactor());
+
+      if (PInvoke.User32.GetWindowRect(hwnd, out PInvoke.RECT rect))
+      {
+        SetWindowPositionAndSize(hwnd, rect.left, rect.top, dpiScaledWidth, rect.bottom - rect.top);
+      }
+    }
+
+    public static void SetHeight(this Window window, int height)
+    {
+      var hwnd = window.GetWindowHandle();
+      var dpiScaledHeight = (int)(height * window.GetDpiScalingFactor());
+
+      if (PInvoke.User32.GetWindowRect(hwnd, out PInvoke.RECT rect))
+      {
+        SetWindowPositionAndSize(hwnd, rect.left, rect.top, rect.right - rect.left, dpiScaledHeight);
+      }
+    }
+
+    //public static void CenterOnScreen(this Window window)
+    //{
+    //  var hwnd = window.GetWindowHandle();
+    //  var monitorHwnd = PInvoke.User32.MonitorFromWindow(hwnd, PInvoke.User32.MonitorOptions.MONITOR_DEFAULTTOPRIMARY);
+    //  if (PInvoke.User32.GetMonitorInfo(monitorHwnd,out PInvoke.User32.MONITORINFO monitorInfo))
+    //  {
+    //    if (PInvoke.User32.GetWindowRect(hwnd, out PInvoke.RECT windowRect))
+    //    {
+    //      var monitorRect = monitorInfo.rcMonitor;
+    //      var monitorWidth = monitorRect.right - monitorRect.left;
+    //      var monitorHeight = monitorRect.bottom - monitorRect.top;
+    //      var windowWidth = windowRect.right - windowRect.left;
+    //      var windowHeight = windowRect.bottom - windowRect.top;
+
+    //      var horizontalSpace = monitorWidth - windowWidth;
+    //      var verticalSpace = monitorHeight - windowHeight;
+
+    //      SetWindowPositionAndSize(hwnd, horizontalSpace / 2, verticalSpace / 2, windowWidth, windowHeight);
+    //    }
+    //  }
+    //}
+
+    public static IntPtr GetWindowHandle(this Window window)
+    {
+      var windowNative = window.As<IWindowNative>();
+      return windowNative.WindowHandle;
+    }
+
+    private static void SetWindowPositionAndSize(IntPtr hwnd, int x, int y, int width, int height)
+    {
+      PInvoke.User32.SetWindowPos(hwnd, PInvoke.User32.SpecialWindowHandles.HWND_TOP,
+                                 x, y, width, height, PInvoke.User32.SetWindowPosFlags.SWP_SHOWWINDOW);
+    }
+
+    private static float GetDpiScalingFactor(this Window window)
+    {
+      var dpi = PInvoke.User32.GetDpiForWindow(window.GetWindowHandle());
+      return (float)dpi / 96;
+    }
+  }
+}
---
+++ WinUI.DesktopApp/WinUI.DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.UI.Xaml;
+using Windows.Foundation;
+using WinUI.Desktop.Extensions;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace WinUI.DesktopApp
+{
+  /// <summary>
+  /// An empty window that can be used on its own or navigated to within a Frame.
+  /// </summary>
+  public sealed partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      this.InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      var rect = new Rect();
+      rect.X = int.TryParse(txtX.Text, out int x) ? x : this.Bounds.X;
+      rect.Y = int.TryParse(txtY.Text, out int y) ? y : this.Bounds.X;
+      rect.Width = int.TryParse(txtWidth.Text, out int width) ? width : this.Bounds.Width;
+      rect.Height = int.TryParse(txtHeight.Text, out int height) ? height : this.Bounds.Height;
+
+      this.SetWidth((int)rect.Width);
+      this.SetHeight((int)rect.Height);
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+      //this.CenterOnScreen();
+    }
+  }
+}
